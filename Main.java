1fe12e7b7938e1a5b6dd7d8dfb80744619b3df1f
AlbertLopezGit <albertolopezarjona@gmail.com>
2021-02-08 16:46:10 +0100

    Modificando archivo Main.java por Albert

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         int uno = 1;
         int dos = 2;
+	int tres = 3;
+	int cuatro = 4;
         int resultado;
-        System.out.println("Buenas Tardes");
+        System.out.println("Buenas Noches");
         System.out.println("Hola, Mundo" );
         System.out.println("Hasta luego" );
-        resultado = uno + dos;
+        resultado = uno + dos + tres + cuatro;
         System.out.println(resultado);
     }
 }
