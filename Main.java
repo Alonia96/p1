d82e6272465048c5fba22642d7e524973fa76aee
Alonia96 <albamartincastella96@gmail.com>
2021-02-08 16:29:02 +0100

    Nueva practica Java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int uno = 1;
+        int dos = 2;
+        int resultado;
+        System.out.println("Buenas Tardes");
+        System.out.println("Hola, Mundo" );
+        System.out.println("Hasta luego" );
+        resultado = uno + dos;
+        System.out.println(resultado);
+    }
+}
