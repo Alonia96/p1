5668763e0d0c2a7a95ce8bc33f715ecff727e2a7
AlbertLopezGit <albertolopezarjona@gmail.com>
2021-02-09 16:43:50 +0100

    otra modificacion

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int uno = 1;
-        int dos = 2;
-	int tres = 3;
-	int cuatro = 4;
+        int uno = 1000;
+        int dos = 2000;
+	int tres = 3000;
+	int cuatro = 4000;
         int resultado;
-        System.out.println("Buenas Noches");
+        System.out.println("HolaHolaHolaHolaHola");
         System.out.println("Hola, Mundo" );
         System.out.println("Hasta luego" );
         resultado = uno + dos + tres + cuatro;
